7fed70f303ebb7a0113a54ac9361257310c479b3
Aaron Smith <gngrwzrd@gmail.com>
2014-12-01 11:57:23 -0800

    Syntax update

--- NSURLRequest+Additions.h
+++ NSURLRequest+Additions.h
@@ -2,7 +2,7 @@
 #import <Foundation/Foundation.h>
 
 @interface NSURLRequest (GWAdditions)
-+ (BOOL)allowsAnyHTTPSCertificateForHost:(NSString*)host;
++ (BOOL) allowsAnyHTTPSCertificateForHost:(NSString *) host;
 
 //Returns an NSURLRequest prepared for an HTTP file upload using standard
 //multipart/form POST data. http://www.cocoadev.com/index.pl?HTTPFileUpload
