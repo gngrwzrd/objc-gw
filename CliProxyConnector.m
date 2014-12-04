865745d9d330b1e08665b9eec291613bb0d9f0ff
Aaron Smith <gngrwzrd@gmail.com>
2014-12-03 16:33:17 -0800

    Updated cli proxy connector, took out GWScrollingTextView which is in it's own repo

--- CliProxyConnector.m
+++ CliProxyConnector.m
@@ -15,18 +15,26 @@
 - (NSDistantObject *) getDistantObjectInApplication:(NSString *) bundleIdentifier withConnectionName:(NSString *) connectionName andLaunchAppIfNotAvailable:(BOOL) launch {
 	NSDistantObject * proxy = [self connectWithName:connectionName];
 	if(proxy) {
-		if(self.distantProtocol)[proxy setProtocolForProxy:self.distantProtocol];
+		if(self.distantProtocol) {
+			[proxy setProtocolForProxy:self.distantProtocol];
+		}
 		return proxy;
 	}
-	if(!launch) return nil;
+	if(!launch) {
+		return nil;
+	}
 	[[NSWorkspace sharedWorkspace] launchAppWithBundleIdentifier:bundleIdentifier options:NSWorkspaceLaunchAsync additionalEventParamDescriptor:nil launchIdentifier:nil];
 	for(int c = 0; proxy==nil && c < 50; c++) {
-		proxy=[self connectWithName:connectionName];
-		if(proxy)break;
+		proxy = [self connectWithName:connectionName];
+		if(proxy) {
+			break;
+		}
 		usleep(15000);
 	}
 	if(proxy) {
-		if(self.distantProtocol)[proxy setProtocolForProxy:self.distantProtocol];
+		if(self.distantProtocol) {
+			[proxy setProtocolForProxy:self.distantProtocol];
+		}
 		return proxy;
 	}
 	return nil;
