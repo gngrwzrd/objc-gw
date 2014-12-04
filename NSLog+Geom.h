0ab5e3a11744f2ffed6c8eff1abf3a90636e9eb1
Aaron Smith <gngrwzrd@gmail.com>
2014-12-04 14:53:10 -0800

    Fixed NSLogSize

--- NSLog+Geom.h
+++ NSLog+Geom.h
@@ -5,7 +5,7 @@
 #define NSLogPoint(point) (NSLog(@"%f %f",point.x,point.y))
 #define NSLogPointWithLabel(label,point) (NSLog(@"%@: %f %f",label,point.x,point.y))
 #define NSLogSize(size) (NSLog(@"%f %f",size.width,size.height))
-#define NSLogSizeWithLabel(label,point) (NSLog(@"%@: %f %f",label,size.width,size.height))
+#define NSLogSizeWithLabel(label,size) (NSLog(@"%@: %f %f",label,size.width,size.height))
 #define NSLogRect(rect) (NSLog(@"%f %f %f %f",rect.origin.x,rect.origin.y,rect.size.width,rect.size.height))
 #define NSLogRectWithLabel(label,rect) (NSLog(@"%@: %f %f %f %f",label,rect.origin.x,rect.origin.y,rect.size.width,rect.size.height))
 
