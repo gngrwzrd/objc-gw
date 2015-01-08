52f6b2be098f0499602365660435c8d2206a51ba
Aaron Smith <gngrwzrd@gmail.com>
2015-01-07 16:11:50 -0800

    Switched program arguments to NSArray

--- LaunchAgentStartupItem.h
+++ LaunchAgentStartupItem.h
@@ -5,11 +5,14 @@
 @property NSString * label;
 @property NSString * fileName;
 @property NSString * executablePath;
-@property NSDictionary * programArguments;
+@property NSArray * programArguments;
 @property (nonatomic) BOOL isInstalled;
 - (id) initWithFileName:(NSString *) fileName label:(NSString *) label; //this uses the main NSBundle executablePath.
 - (id) initWithExecutablePath:(NSString *) executablePath fileName:(NSString *) fileName label:(NSString *) label;
 - (void) install;
 - (void) uninstall;
+
+//if the launch agent plist is installed and the executable path is not the same
+//as self.executablePath, it's uninstalled, and re-installed with the new path.
 - (void) updateExecutablePath;
 @end
